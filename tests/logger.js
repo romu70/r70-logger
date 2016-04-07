bcc8aa177b6d51805154779ab1cab56e6da3ad2a
romu70 <r@tisserand.eu>
2016-04-07 17:38:45 +0200

    First tests created.

--- tests/logger.js
+++ tests/logger.js
@@ -1,8 +1,6 @@
 var test = require('tape');
 var logger = require("../src/logger.js");
 
-//TODO: logger is not initialized at first time
-//TODO: logger init doesn't accept null or empty
 //TODO: logger file path must be a string
 //TODO: logger file path must exist
 //TODO: if correct input, the logger must be initialized
@@ -12,8 +10,8 @@ test("Logger initialization", function (t) {
     t.plan(3);
 
     t.equal(false, logger.initialized, "logger is not initialized at first time");
-    t.throws(logger.init(), null, "logger init doesn't accept null or empty");
-    t.throws(logger.init(null), null, "logger init doesn't accept null or empty");
+    t.throws(logger.init, null, "logger init doesn't accept null or empty");
+    t.throws(function() { logger.init(null) }, null, "logger init doesn't accept null or empty");
  
 //     t.equal(typeof Date.now, 'function');
 //     var start = Date.now();
