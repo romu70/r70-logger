02fc21424d166dcc6b2dda872df5dba7a8261aa5
romu70 <r@tisserand.eu>
2016-04-11 18:01:47 +0200

    Added more input check and related test cases.

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// const assert = require('assert');
-// const utils = require('./utils.js');
+const path = require('path');
+const fs  = require('fs');
 
 // private stuff here
 
@@ -13,16 +13,23 @@ module.exports = {
   // Initializes the new logger
   // 
   // Inputs:
-  // - path: the log file full path
-  init: function (path) {
+  // - logfile: the log file full logfile
+  init: function (logfile) {
 
-    console.log(`path: ${path}`);
-    console.log(`path type: ${typeof path}`);
+     //console.log(`logfile: ${typeof logfile}`);
+    // console.log(`logfile type: ${typeof logfile}`);
 
-    if(path === undefined || path === null)
-      throw new TypeError("Logger: the path can't be null");
-    if(typeof path === string)
-      throw new TypeError("Logger: the path must be a string");
+    if(logfile === undefined || logfile === null)
+      throw new TypeError("Logger: the logfile can't be null");
+    if(typeof logfile !== "string")
+      throw new TypeError("Logger: the logfile must be a string");
+
+    // Check the log file folder exists
+    let dir = path.dirname(logfile);
+    fs.accessSync(dir, fs.F_OK);
+    // Check the log file folder is writable
+    dir += path.sep;
+    fs.accessSync(dir, fs.W_OK);      
   },
   
   // getter of: initialized 
--- tests/logger.js
+++ tests/logger.js
@@ -1,19 +1,24 @@
 var test = require('tape');
+var fs = require('fs');
 var logger = require("../src/logger.js");
 
-//TODO: logger file path must be a string
-//TODO: logger file path must exist
 //TODO: if correct input, the logger must be initialized
 //TODO: if initialized, a second init call must raise an exception
 
 test("Logger initialization", function (t) {
-    t.plan(4);
+    t.plan(6);
 
     t.equal(false, logger.initialized, "logger is not initialized at first time");
     t.throws(logger.init, null, "logger init doesn't accept null or empty");
-    t.throws(function() { logger.init(null) }, null, "logger init doesn't accept null or empty");
-    t.throws(function() { logger.init(3) }, null, "logger file path must be a string");
+    t.throws(function() { logger.init(null); }, null, "logger init doesn't accept null or empty");
+    t.throws(function() { logger.init(3); }, null, "logger file path must be a string");
+    t.throws(function() { logger.init("/titi/toto/tutu"); }, null, "logger file path must exist");
+    t.throws(function() { logger.init("./toto.log"); }, null, "logger file path must be writable");
  
+    fs.writeFile("./toto.log", 'Hello Node.js', (err) => {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
 //     t.equal(typeof Date.now, 'function');
 //     var start = Date.now();
 // 
