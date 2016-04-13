356db4b9ab9da01f2783d5eb4050b358c13bcf4c
romu70 <r@tisserand.eu>
2016-04-13 17:48:32 +0200

    Completed the logger initialization and related tests.

--- src/logger.js
+++ src/logger.js
@@ -5,8 +5,8 @@ const fs  = require('fs');
 
 // private stuff here
 
-// If true, this means the logger has already been initialized 
-var initialized = false;
+// File descriptor of the log file. If it's not null, that means the log has been initialized. 
+var fd = null;
 
 // Returns a single object, this is a singleton!
 module.exports = {
@@ -16,9 +16,8 @@ module.exports = {
   // - logfile: the log file full logfile
   init: function (logfile) {
 
-     //console.log(`logfile: ${typeof logfile}`);
-    // console.log(`logfile type: ${typeof logfile}`);
-
+    if(fd !== null)
+      throw new Error("Logger is already initialized");
     if(logfile === undefined || logfile === null)
       throw new TypeError("Logger: the logfile can't be null");
     if(typeof logfile !== "string")
@@ -29,11 +28,23 @@ module.exports = {
     fs.accessSync(dir, fs.F_OK);
     // Check the log file folder is writable
     dir += path.sep;
-    fs.accessSync(dir, fs.W_OK);      
+    fs.accessSync(dir, fs.W_OK);
+    
+    // At this point, all is ok, we should be able to open the log file.
+    fd = fs.openSync(logfile, 'w');      
   },
   
-  // getter of: initialized 
-  get initialized() {
-    return initialized;
+  // Close this log
+  close: function() {
+    fs.closeSync(fd);
+    fd = null;
+  },
+  
+  isInitialized: function() {
+    return fd !== null;
+  },
+  
+  get fd() {
+    return fd;
   }
 }
--- tests/logger.js
+++ tests/logger.js
@@ -5,34 +5,20 @@ const fs = require('fs');
 const path = require('path');
 const logger = require("../src/logger.js");
 
-//TODO: if correct input, the logger must be initialized
 //TODO: if initialized, a second init call must raise an exception
 
-test("Logger initialization", function (t) {
-    t.plan(6);
+test("Logger opening/closing", function (t) {
+    t.plan(11);
 
-    t.equal(false, logger.initialized, "logger is not initialized at first time");
+    t.equal(false, logger.isInitialized(), "logger is not initialized at first time");
     t.throws(logger.init, null, "logger init doesn't accept null or empty");
     t.throws(function() { logger.init(null); }, null, "logger init doesn't accept null or empty");
     t.throws(function() { logger.init(3); }, null, "logger file path must be a string");
     t.throws(function() { logger.init("/titi/toto/tutu"); }, null, "logger file path must exist");
     t.throws(function() { logger.init("/should-not-work.log"); }, null, "logger file path must be writable");
- 
-//     // Check the log file folder exists
-//     let dir = path.dirname("./toto.log");
-//     fs.accessSync(dir, fs.F_OK);
-//     // Check the log file folder is writable
-//     dir += path.sep;
-//     fs.accessSync(dir, fs.W_OK);      
-// 
-//     fs.writeFile("./toto.log", 'Hello Node.js', (err) => {
-//       if (err) throw err;
-//       console.log('It\'s saved!');
-//     });
-//     t.equal(typeof Date.now, 'function');
-//     var start = Date.now();
-// 
-//     setTimeout(function () {
-//         t.equal(Date.now() - start, 100);
-//     }, 100);
+    t.doesNotThrow(function() { logger.init("./should-work.log"); }, null, "this should pass");
+    t.assert(logger.isInitialized(), "logger must be initialized");
+    t.throws(function() { logger.init("./should-not-work.log"); }, null, "logger can be initialized only once");
+    t.doesNotThrow(function() { logger.close(); }, null, "this should pass");
+    t.equal(false, logger.isInitialized(), "logger is not initialized after closing");
 });
