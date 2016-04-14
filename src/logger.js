9c96ce8582da9f5eb77d00a6d77c97b876d06c0c
romu70 <r@tisserand.eu>
2016-04-14 18:01:02 +0200

    Started to work on the log functions.

--- src/logger.js
+++ src/logger.js
@@ -9,6 +9,20 @@ const fs  = require('fs');
 // File descriptor of the log file. If it's not null, that means the log has been initialized. 
 var fd = null;
 
+// Write function which actually writes the message into the log file
+function log(level, msg) {
+  if(msg === undefined || msg === null)
+    throw new TypeError("Logger.log: the msg can't be null");
+    
+
+}
+
+// Returns a string of now, but formatted for the log
+function formatDateTime() {
+  let now = Date.now();
+  return `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}:${now.getMilliseconds()}`;
+}
+
 // Returns a single object, this is a singleton!
 module.exports = {
   // Initializes the new logger
@@ -18,11 +32,11 @@ module.exports = {
   init: function (logfile) {
 
     if(fd !== null)
-      throw new Error("Logger is already initialized");
+      throw new Error("Logger.init is already initialized");
     if(logfile === undefined || logfile === null)
-      throw new TypeError("Logger: the logfile can't be null");
+      throw new TypeError("Logger.init: the logfile can't be null");
     if(typeof logfile !== "string")
-      throw new TypeError("Logger: the logfile must be a string");
+      throw new TypeError("Logger.init: the logfile must be a string");
 
     // Check the log file folder exists
     let dir = path.dirname(logfile);
@@ -41,11 +55,23 @@ module.exports = {
     fd = null;
   },
   
+  // Returns true if the log file has been already opened.
   isInitialized: function() {
     return fd !== null;
   },
   
-  get fd() {
-    return fd;
-  }
+  // Log the specified message to the file
+  message: function(msg) {
+    log("Info", msg);
+  },
+  
+  // Log the specified warning to the file
+  warning: function(msg) {
+    log("Warning", msg);
+  },
+  
+  // Log the specified error to the file
+  error: function(msg) {
+    log("ERROR", msg);    
+  },
 }
