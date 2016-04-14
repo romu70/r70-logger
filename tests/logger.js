c6fb1c89b077c123de1a7c12cb6086a6eddd62dd
romu70 <r@tisserand.eu>
2016-04-14 17:16:21 +0200

    Added tests about functions existence.

--- tests/logger.js
+++ tests/logger.js
@@ -6,9 +6,11 @@ const path = require('path');
 const logger = require("../src/logger.js");
 
 test("Logger opening/closing", function (t) {
-    t.plan(11);
+    t.plan(14);
 
+    t.equal(true, typeof logger.isInitialized === 'function', "function 'isInitialized' must exist");
     t.equal(false, logger.isInitialized(), "logger is not initialized at first time");
+    t.equal(true, typeof logger.init === 'function', "function 'init' must exist");
     t.throws(logger.init, null, "logger init doesn't accept null or empty");
     t.throws(function() { logger.init(null); }, null, "logger init doesn't accept null or empty");
     t.throws(function() { logger.init(3); }, null, "logger file path must be a string");
@@ -17,6 +19,7 @@ test("Logger opening/closing", function (t) {
     t.doesNotThrow(function() { logger.init("./should-work.log"); }, null, "this should pass");
     t.assert(logger.isInitialized(), "logger must be initialized");
     t.throws(function() { logger.init("./should-not-work.log"); }, null, "logger can be initialized only once");
+    t.equal(true, typeof logger.close === 'function', "function 'close' must exist");
     t.doesNotThrow(function() { logger.close(); }, null, "this should pass");
     t.equal(false, logger.isInitialized(), "logger is not initialized after closing");
 });
